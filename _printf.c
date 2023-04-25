658cf84906366a59b63d33823bea8837ffafedba
iamthatnikah <kobbyaryeetey@hotmail.com>
2023-04-25 05:31:35 -0700

    File Updated

--- _printf.c
+++ _printf.c
@@ -4,48 +4,48 @@
  * _printf - Function to format output conversion and print data.
  * @format: input string.
  *
- * Return: chars printed.
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	unsigned int x = 0, len = 0, ibuf = 0;
+	unsigned int i = 0, len = 0, ibuf = 0;
 	va_list arguments;
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
 
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[x] == '%' && !format[x + 1]))
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-	if (!format[x])
+	if (!format[i])
 		return (0);
-	for (x = 0; format && format[x]; x++)
+	for (i = 0; format && format[i]; i++)
 	{
-		if (format[x] == '%')
+		if (format[i] == '%')
 		{
-			if (format[x + 1] == '\0')
+			if (format[i + 1] == '\0')
 			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 				return (-1);
 			}
 			else
-			{	function = get_print_func(format, x + 1);
+			{	function = get_print_func(format, i + 1);
 				if (function == NULL)
 				{
-					if (format[x + 1] == ' ' && !format[x + 2])
+					if (format[i + 1] == ' ' && !format[i + 2])
 						return (-1);
-					handl_buf(buffer, format[x], ibuf), len++, x--;
+					handl_buf(buffer, format[i], ibuf), len++, i--;
 				}
 				else
 				{
 					len += function(arguments, buffer, ibuf);
-					x += ev_print_func(format, x + 1);
+					i += ev_print_func(format, i + 1);
 				}
-			} x++;
+			} i++;
 		}
 		else
-			handl_buf(buffer, format[x], ibuf), len++;
-		for (xbuf = len; xbuf > 1024; xbuf -= 1024)
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
 			;
 	}
-	print_buf(buffer, xbuf), free(buffer), va_end(arguments);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);
-}
+}}
